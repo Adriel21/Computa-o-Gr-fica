f9a4bd0062b609450bbb5ed044b141ab01bd87cf
Adriel Araujo <adrielcontact@gmail.com>
2019-06-29 02:07:26 -0300

    Create Musicas.h

---
+++ Musicas.h
@@ -0,0 +1,15 @@
+#ifndef MUSICAS_UKULELEHERO_H
+#define MUSICAS_UKULELEHERO_H
+#include <GL/glut.h>
+#include <stdio.h>
+
+void desenhaNotaVerde(int posZ);
+void desenhaNotaVermelha(int posZ);
+void desenhaNotaAmarela(int posZ);
+void desenhaNotaAzul(int posZ);
+
+void MusicaEASY();
+void MusicaMEDIUM_BIB();
+void MusicaHARD();
+
+#endif //MUSICAS_UKULELEHERO_H
