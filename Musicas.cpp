a91b294943af8bc7a5b4253ea90a6602bd0b35c1
Adriel Araujo <adrielcontact@gmail.com>
2019-06-29 02:06:57 -0300

    Create Musicas.cpp

---
+++ Musicas.cpp
@@ -0,0 +1,279 @@
+//Dispõe das musicas (sequências de notas musicais, usadas no jogo "Ukulele Hero"
+
+#include <GL/glut.h>
+#include "musicas.h"
+
+void MusicaEASY(){
+	desenhaNotaVerde(-10);
+	desenhaNotaVermelha(-14);
+	desenhaNotaAmarela(-19);
+	desenhaNotaAzul(-25);
+
+	desenhaNotaVerde(-30);
+	desenhaNotaVermelha(-33);
+	desenhaNotaAmarela(-40);
+	desenhaNotaAzul(-43);
+
+	desenhaNotaVerde(-48);
+	desenhaNotaVerde(-52);
+	desenhaNotaVerde(-59);
+	desenhaNotaVerde(-65);
+	desenhaNotaVerde(-70);
+	desenhaNotaVermelha(-76);
+	desenhaNotaAmarela(-80);
+	desenhaNotaAzul(-89);
+
+	desenhaNotaVermelha(-95);
+	desenhaNotaVerde(-102);
+	desenhaNotaAzul(-106);
+	desenhaNotaVerde(-111);
+	desenhaNotaVermelha(-117);
+	desenhaNotaVermelha(-125);
+	desenhaNotaAmarela(-130);
+
+	desenhaNotaAzul(-133);
+	desenhaNotaVerde(-140);
+	desenhaNotaVermelha(-144);
+	desenhaNotaVermelha(-149);
+	desenhaNotaAzul(-155);
+	desenhaNotaVerde(-159);
+
+	desenhaNotaVerde(-165);
+	desenhaNotaVermelha(-170);
+	desenhaNotaAzul(-176);
+	desenhaNotaVerde(-180);
+	desenhaNotaVermelha(-184);
+	desenhaNotaVerde(-189);
+	desenhaNotaVermelha(-193);
+	desenhaNotaAzul(-198);
+	desenhaNotaVerde(-207);
+
+	desenhaNotaVermelha(-213);
+	desenhaNotaAmarela(-218);
+	desenhaNotaAzul(-221);
+	desenhaNotaVerde(-226);
+	desenhaNotaVermelha(-231);
+	desenhaNotaVermelha(-235);
+
+	desenhaNotaAmarela(-240);
+	desenhaNotaAzul(-243);
+	desenhaNotaVerde(-248);
+
+}
+
+void MusicaMEDIUM_BIB(){
+
+	desenhaNotaAmarela(-5);
+	desenhaNotaVermelha(-13);
+	desenhaNotaVerde(-14);
+	desenhaNotaVermelha(-15);
+	desenhaNotaVerde(-23);
+	desenhaNotaVermelha(-24);
+	desenhaNotaVerde(-25);
+
+	desenhaNotaAzul(-36);
+	desenhaNotaAmarela(-37);
+	desenhaNotaVermelha(-38);
+	desenhaNotaVerde(-39);
+	desenhaNotaVermelha(-41);
+	desenhaNotaAmarela(-41);
+	desenhaNotaVerde(-42);
+
+	desenhaNotaAmarela(-46);
+	desenhaNotaVermelha(-54);
+	desenhaNotaVerde(-55);
+	desenhaNotaVermelha(-56);
+	desenhaNotaVerde(-64);
+	desenhaNotaVermelha(-65);
+	desenhaNotaVerde(-66);
+
+	desenhaNotaAzul(-72);
+	desenhaNotaVerde(-74);
+	desenhaNotaAzul(-76);
+	desenhaNotaVermelha(-78);
+	desenhaNotaAzul(-80);
+	desenhaNotaAmarela(-82);
+	desenhaNotaAzul(-84);
+	desenhaNotaAzul(-86);
+
+	desenhaNotaAmarela(-92);
+	desenhaNotaVerde(-96);
+	desenhaNotaAzul(-101);
+	desenhaNotaAmarela(-104);
+	desenhaNotaAzul(-108);
+
+	desenhaNotaAmarela(-114);
+	desenhaNotaVerde(-118);
+	desenhaNotaAzul(-123);
+	desenhaNotaAmarela(-126);
+	desenhaNotaAzul(-130);
+
+	desenhaNotaAzul(-136);
+	desenhaNotaVerde(-140);
+	desenhaNotaAmarela(-144);
+	desenhaNotaVermelha(-148);
+	desenhaNotaAmarela(-152);
+
+	desenhaNotaAzul(-158);
+	desenhaNotaVerde(-162);
+	desenhaNotaAmarela(-166);
+	desenhaNotaVermelha(-169);
+	desenhaNotaAmarela(-173);
+
+	desenhaNotaAmarela(-179);
+	desenhaNotaVerde(-183);
+	desenhaNotaAzul(-188);
+	desenhaNotaAmarela(-191);
+	desenhaNotaAzul(-195);
+
+	desenhaNotaAmarela(-201);
+	desenhaNotaVerde(-205);
+	desenhaNotaAzul(-210);
+	desenhaNotaAmarela(-213);
+	desenhaNotaAzul(-217);
+
+	desenhaNotaVerde(-221);
+	desenhaNotaVermelha(-222);
+	desenhaNotaVerde(-224);
+	desenhaNotaAzul(-244);
+	desenhaNotaAmarela(-247);
+	desenhaNotaVermelha(-250);
+
+}
+
+void MusicaHARD(){
+	desenhaNotaVerde(-10);
+	desenhaNotaVermelha(-12);
+	desenhaNotaVerde(-13);
+	desenhaNotaAzul(-14);
+	desenhaNotaAmarela(-16);
+	desenhaNotaAmarela(-17);
+
+	desenhaNotaVerde(-20);
+	desenhaNotaAzul(-21);
+	desenhaNotaAzul(-22);
+	desenhaNotaVerde(-24);
+	desenhaNotaVermelha(-25);
+	desenhaNotaAmarela(-28);
+
+	desenhaNotaVermelha(-32);
+	desenhaNotaAmarela(-36);
+	desenhaNotaAzul(-37);
+	desenhaNotaVerde(-38);
+	desenhaNotaVermelha(-40);
+	desenhaNotaAmarela(-42);
+	desenhaNotaAmarela(-43);
+
+	desenhaNotaVermelha(-44);
+	desenhaNotaVerde(-46);
+	desenhaNotaAzul(-48);
+	desenhaNotaVerde(-49);
+	desenhaNotaVermelha(-52);
+	desenhaNotaVermelha(-53);
+	desenhaNotaAmarela(-55);
+
+	desenhaNotaAzul(-57);
+	desenhaNotaVerde(-60);
+	desenhaNotaVermelha(-63);
+	desenhaNotaVermelha(-64);
+	desenhaNotaAzul(-66);
+	desenhaNotaVerde(-67);
+
+	desenhaNotaVerde(-68);
+	desenhaNotaVermelha(-70);
+	desenhaNotaAzul(-74);
+	desenhaNotaVerde(-75);
+	desenhaNotaVermelha(-77);
+	desenhaNotaVerde(-78);
+	desenhaNotaVermelha(-79);
+	desenhaNotaAzul(-84);
+	desenhaNotaVerde(-86);
+
+	desenhaNotaVermelha(-88);
+	desenhaNotaAmarela(-89);
+	desenhaNotaAzul(-90);
+	desenhaNotaVerde(-93);
+	desenhaNotaVermelha(-94);
+	desenhaNotaVermelha(-99);
+
+	desenhaNotaVerde(-101);
+	desenhaNotaVermelha(-102);
+	desenhaNotaVerde(-105);
+	desenhaNotaAzul(-106);
+	desenhaNotaAmarela(-107);
+	desenhaNotaAmarela(-110);
+
+	desenhaNotaVerde(-113);
+	desenhaNotaAzul(-115);
+	desenhaNotaAzul(-116);
+	desenhaNotaVerde(-118);
+	desenhaNotaVermelha(-121);
+	desenhaNotaAmarela(-123);
+
+	desenhaNotaVermelha(-125);
+	desenhaNotaAmarela(-127);
+	desenhaNotaAzul(-128);
+	desenhaNotaVerde(-131);
+	desenhaNotaVermelha(-134);
+	desenhaNotaAmarela(-139);
+	desenhaNotaAmarela(-141);
+
+	desenhaNotaVermelha(-144);
+	desenhaNotaVerde(-146);
+	desenhaNotaAzul(-149);
+	desenhaNotaVermelha(-152);
+	desenhaNotaVermelha(-153);
+	desenhaNotaAmarela(-155);
+
+	desenhaNotaAzul(-157);
+	desenhaNotaVerde(-160);
+	desenhaNotaVermelha(-163);
+	desenhaNotaVermelha(-164);
+	desenhaNotaAzul(-166);
+	desenhaNotaVerde(-167);
+
+	desenhaNotaVerde(-168);
+	desenhaNotaVermelha(-170);
+	desenhaNotaAzul(-174);
+	desenhaNotaVerde(-175);
+	desenhaNotaVermelha(-177);
+	desenhaNotaVerde(-178);
+	desenhaNotaVermelha(-179);
+	desenhaNotaAzul(-184);
+	desenhaNotaVerde(-186);
+
+	desenhaNotaVermelha(-188);
+	desenhaNotaAmarela(-189);
+	desenhaNotaAzul(-190);
+	desenhaNotaVerde(-193);
+	desenhaNotaVermelha(-194);
+	desenhaNotaVermelha(-199);
+
+	desenhaNotaVerde(-204);
+	desenhaNotaVermelha(-207);
+	desenhaNotaVerde(-210);
+	desenhaNotaAzul(-214);
+	desenhaNotaAmarela(-216);
+	desenhaNotaAmarela(-217);
+
+	desenhaNotaVerde(-220);
+	desenhaNotaAzul(-221);
+	desenhaNotaAzul(-222);
+	desenhaNotaVerde(-224);
+	desenhaNotaVermelha(-225);
+	desenhaNotaAmarela(-228);
+
+	desenhaNotaVermelha(-232);
+	desenhaNotaAmarela(-236);
+	desenhaNotaAzul(-237);
+	desenhaNotaVerde(-238);
+	desenhaNotaVermelha(-240);
+	desenhaNotaAmarela(-242);
+	desenhaNotaAmarela(-243);
+
+	desenhaNotaVermelha(-244);
+	desenhaNotaVerde(-246);
+	desenhaNotaAzul(-248);
+	desenhaNotaVerde(-249);
+
+}
